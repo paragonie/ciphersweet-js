29ed72dab87932e18f1af960da515fd53ed96099
Paragon Initiative Enterprises <security@paragonie.com>
2019-05-02 12:14:48 -0400

    Rewrite using async everywhere (and lowercase file names)

---
+++ test/fipscrypto-test.js
@@ -0,0 +1,56 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+const FIPSCrypto = require('../lib/backend/fipsrypto');
+const SymmetricKey = require('../lib/backend/key/SymmetricKey');
+const sodium = require('sodium-native');
+
+describe('FIPSCrypto Tests', function () {
+    it('Encrypts and decrypts successfully', function () {
+        this.timeout(5000);
+        let random_buf = Buffer.alloc(32, 0);
+        sodium.randombytes_buf(random_buf);
+        let fips = new FIPSCrypto();
+        let key = new SymmetricKey(random_buf);
+        let plaintext, exampleKey, exampleCipher;
+
+        plaintext = 'This is a secret message';
+        fips.encrypt(plaintext, key).then(
+            (encrypted) => {
+                fips.decrypt(encrypted, key).then(
+                    (decrypted) => {
+                        expect(decrypted).to.be.equal(plaintext);
+                    }
+                );
+            }
+        );
+
+
+        fips.encrypt(plaintext, key, 'test aad')
+            .then(encrypted => {
+                let caught = false;
+                fips.decrypt(encrypted, key)
+                    .catch((e) => {
+                        caught = true;
+                        expect(e.message).to.be.equal('Invalid MAC');
+                    })
+                    .then(() => {
+                        if (!caught) {
+                            assert(null, 'AAD not being used in calculation');
+                        }
+                    });
+
+                fips.decrypt(encrypted, key, 'test aad').then(
+                    (decrypted) => {
+                        expect(decrypted).to.be.equal(plaintext);
+                    }
+                );
+            });
+
+        exampleKey = Buffer.from('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', 'hex');
+        exampleCipher = 'fips:JkzlZgUUdwo6XDRYSKNTnuWMDVcIa7M4R0Xtg1c3aD14ZUiu5YGTiGu9PC2SAjRAZTTurWYa1KfrMJKSncc0llwcNeyEsWMytOir8oqskQtIF0XEkjTJEJSjxmkerxRfHNyBnOimLZ6fg31IjLWrzOW1UX3ARRwSjabK';
+        fips.decrypt(exampleCipher, exampleKey).then(exampleDecrypt => {
+            expect(exampleDecrypt.toString('utf-8')).to.be.equal('This is just a test message');
+        });
+
+    });
+});
